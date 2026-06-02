c7a4a1e4521acb0f5ddd02885dde1eddbefd4fd2
Kevin Manrique Campos Granados <USSS027225@ugb.edu.sv>
2026-06-01 21:57:04 -0600

    Permisos en android para la app de mesanjeria, Funcionalidad completa del registro de amigos en firebase y la subida de fotos a storage

--- app/src/main/java/com/example/labo/detectarinternet.java
+++ app/src/main/java/com/example/labo/detectarinternet.java
@@ -1,13 +1,17 @@
 package com.example.labo;
+import android.Manifest;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+
+import androidx.annotation.RequiresPermission;
 
 public class detectarinternet {
     Context context;
     public detectarinternet(Context context) {
         this.context = context;
     }
+    @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
     public boolean hayConexionInternet(){
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
